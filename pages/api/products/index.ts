2b65dc44da6a461ae2704aeffc5b3d62604a7811
ale-strada <79310707+ale-strada@users.noreply.github.com>
2022-09-26 16:42:01 -0300

    objeto res products

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -5,6 +5,23 @@ import { productsIndex } from "lib/algolia";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   const { offset, limit } = getOffsetAndLimitFromReq(req, 100, 10000);
-  const results = await productsIndex.search(req.query.search as string);
-  res.send(results);
+
+  // index.search('query', {
+  //   offset: realOffset,
+  //   length: realLimit:
+  // }).then(({ hits }) => {
+  //   console.log(hits);
+  // });
+  const results = await productsIndex.search(req.query.search as string, {
+    offset: offset,
+    length: limit,
+  });
+  res.send({
+    results: results.hits,
+    pagination: {
+      limit,
+      offset,
+      total: results.nbHits,
+    },
+  });
 }
