d5fa3bd2f0f1666f1cd146af68ddcd03e210b3c8
ale-strada <79310707+ale-strada@users.noreply.github.com>
2022-09-26 14:41:36 -0300

    ajuste endpoints

--- pages/api/me/address.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import methods from "micro-method-router";
-
-export default methods({
-  async patch(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).send("me address" + JSON.stringify(req.body));
-  },
-});
--- pages/api/order/index.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import methods from "micro-method-router";
-
-export default methods({
-  async post(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).send("ORDER, URL mercadopago" + JSON.stringify(req.query));
-  },
-});
